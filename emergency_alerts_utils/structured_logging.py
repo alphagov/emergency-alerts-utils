72c9fd38ed4e701285d9fd7307b25bc7a20cf6c7
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-02-21 09:46:43 +0000

    get log group name from env var

--- emergency_alerts_utils/structured_logging.py
+++ emergency_alerts_utils/structured_logging.py
@@ -12,7 +12,7 @@ from uuid import UUID
 b3client = boto3.client("logs", region_name=os.environ.get('AWS_REGION'))
 try:
     b3client.create_log_stream(
-        logGroupName='/ecs/eas-app-api-container',
+        logGroupName=os.environ.get('LOG_GROUP_NAME'),
         logStreamName=os.environ.get('HOSTNAME')
     )
 except ClientError as e:
