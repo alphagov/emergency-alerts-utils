d782f5e7a5a8b83684cc55c953d819022f7fa090
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-09-01 15:10:06 +0100

    remove obsolete tests

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -1,61 +1,62 @@
 import json
 import logging as builtin_logging
-import logging.handlers as builtin_logging_handlers
 
-import pytest
+# import pytest
 import pythonjsonlogger.jsonlogger
 
 from emergency_alerts_utils import logging
 
-
-def test_get_handlers_sets_up_logging_appropriately_with_debug(tmpdir):
-    class App:
-        config = {"NOTIFY_LOG_PATH": str(tmpdir / "foo"), "NOTIFY_APP_NAME": "bar", "NOTIFY_LOG_LEVEL": "ERROR"}
-        debug = True
-
-    app = App()
-
-    handlers = logging.get_handlers(app)
-
-    assert len(handlers) == 1
-    assert type(handlers[0]) == builtin_logging.StreamHandler
-    assert type(handlers[0].formatter) == logging.CustomLogFormatter
-    assert not (tmpdir / "foo").exists()
+# import logging.handlers as builtin_logging_handlers
 
 
-@pytest.mark.parametrize(
-    "platform",
-    [
-        "local",
-        "paas",
-        "something-else",
-    ],
-)
-def test_get_handlers_sets_up_logging_appropriately_without_debug_when_not_on_ecs(tmpdir, platform):
-    class App:
-        config = {
-            # make a tempfile called foo
-            "NOTIFY_LOG_PATH": str(tmpdir / "foo"),
-            "NOTIFY_APP_NAME": "bar",
-            "NOTIFY_LOG_LEVEL": "ERROR",
-            "NOTIFY_RUNTIME_PLATFORM": platform,
-        }
-        debug = False
+# def test_get_handlers_sets_up_logging_appropriately_with_debug(tmpdir):
+#     class App:
+#         config = {"NOTIFY_LOG_PATH": str(tmpdir / "foo"), "NOTIFY_APP_NAME": "bar", "NOTIFY_LOG_LEVEL": "ERROR"}
+#         debug = True
 
-    app = App()
+#     app = App()
 
-    handlers = logging.get_handlers(app)
+#     handlers = logging.get_handlers(app)
 
-    assert len(handlers) == 2
-    assert type(handlers[0]) == builtin_logging.StreamHandler
-    assert type(handlers[0].formatter) == pythonjsonlogger.jsonlogger.JsonFormatter
+#     assert len(handlers) == 1
+#     assert type(handlers[0]) == builtin_logging.StreamHandler
+#     assert type(handlers[0].formatter) == logging.CustomLogFormatter
+#     assert not (tmpdir / "foo").exists()
 
-    assert type(handlers[1]) == builtin_logging_handlers.WatchedFileHandler
-    assert type(handlers[1].formatter) == pythonjsonlogger.jsonlogger.JsonFormatter
 
-    dir_contents = tmpdir.listdir()
-    assert len(dir_contents) == 1
-    assert dir_contents[0].basename == "foo.json"
+# @pytest.mark.parametrize(
+#     "platform",
+#     [
+#         "local",
+#         "paas",
+#         "something-else",
+#     ],
+# )
+# def test_get_handlers_sets_up_logging_appropriately_without_debug_when_not_on_ecs(tmpdir, platform):
+#     class App:
+#         config = {
+#             # make a tempfile called foo
+#             "NOTIFY_LOG_PATH": str(tmpdir / "foo"),
+#             "NOTIFY_APP_NAME": "bar",
+#             "NOTIFY_LOG_LEVEL": "ERROR",
+#             "NOTIFY_RUNTIME_PLATFORM": platform,
+#         }
+#         debug = False
+
+#     app = App()
+
+#     handlers = logging.get_handlers(app)
+
+#     assert len(handlers) == 2
+#     assert type(handlers[0]) == builtin_logging.StreamHandler
+#     assert type(handlers[0].formatter) == pythonjsonlogger.jsonlogger.JsonFormatter
+
+#     assert type(handlers[1]) == builtin_logging_handlers.WatchedFileHandler
+#     assert type(handlers[1].formatter) == pythonjsonlogger.jsonlogger.JsonFormatter
+
+#     dir_contents = tmpdir.listdir()
+#     assert len(dir_contents) == 1
+#     assert dir_contents[0].basename == "foo.json"
 
 
 def test_get_handlers_sets_up_logging_appropriately_without_debug_on_ecs(tmpdir):
