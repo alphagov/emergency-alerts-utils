155ff136443e00732f8eb2a8d36ce37ed3fd2d5c
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-09-15 14:56:16 +0100

    logging

--- emergency_alerts_utils/logging.py
+++ emergency_alerts_utils/logging.py
@@ -13,9 +13,6 @@ def init_app(app, statsd_client=None):
 
     configure_application_logger(app)
 
-    # app.logger.addHandler(_configure_root_handler(app))
-    # app.logger.setLevel(logging.getLevelName(app.config["NOTIFY_LOG_LEVEL"]))
-
     app.logger.info("Logging configured")
 
 
@@ -35,82 +32,14 @@ def configure_application_logger(app):
     app.logger.setLevel(logging.getLevelName(app.config["NOTIFY_LOG_LEVEL"]))
 
 
-# def configure_celery_logger(app):
-#     handler = logging.StreamHandler(sys.stdout)
-#     handler.setLevel(logging.getLevelName(app.config["NOTIFY_LOG_LEVEL"]))
-#     handler.setFormatter(JsonFormatter())
-#     handler.addFilter(SuppressTracebackFilter())
-#     app.logger.addHandler(handler)
-#     app.logger.setLevel(logging.getLevelName(app.config["NOTIFY_LOG_LEVEL"]))
-
-
-# def _configure_root_handler(app):
-#     handler = logging.StreamHandler(sys.stdout)
-#     handler.setLevel(logging.getLevelName(app.config["NOTIFY_LOG_LEVEL"]))
-#     handler.setFormatter(JsonFormatter())
-#     handler.addFilter(AppNameFilter(app.config["NOTIFY_APP_NAME"]))
-#     handler.addFilter(RequestIdFilter())
-#     handler.addFilter(ServiceIdFilter())
-#     handler.addFilter(SuppressTracebackFilter())
-#     return handler
-
-
-# def configure_notraceback_logger(level, logger):
-#     logger.addHandler(_configure_notraceback_handler(level))
-#     logger.setLevel(logging.getLevelName(level))
-#     logger.propagate = False
-
-
-# def _configure_notraceback_handler(level):
-#     handler = logging.StreamHandler(sys.stdout)
-#     handler.setLevel(logging.getLevelName(level))
-#     handler.setFormatter(NoExceptionFormatter())
-#     handler.addFilter(SuppressTracebackFilter())
-#     return handler
-
-
-# def configure_json_logger(level, logger):
-#     logger.addHandler(_configure_json_handler(level))
-#     logger.setLevel(logging.getLevelName(level))
-#     logger.propagate = False
-
-
-# def _configure_json_handler(level):
-#     handler = logging.StreamHandler(sys.stdout)
-#     handler.setLevel(logging.getLevelName(level))
-#     handler.setFormatter(JsonFormatter)
-#     return handler
-
-
 class JsonFormatterForCloudWatch(JsonFormatter):
     def formatException(self, exc_info):
         """
-        Format an exception so that it prints on a single line.
+        Replace '\n' with '\r' to prevent CloudWatch adding
+        each newline as a separate log entry
         """
         result = super().formatException(exc_info)
-        return result.replace("\n", "")  # or format into one line however you want to
-
-    # def format(self, record):
-    #     s = super().format(record)
-    #     if record.exc_text:
-    #         s = s.replace('\n', '') + '|'
-    #     return s
-
-
-class SuppressTracebackFilter(logging.Filter):
-    def filter(self, record):
-        record.exc_info = False
-        record.exc_text = None
-        return True
-
-
-class NoExceptionFormatter(logging.Formatter):
-    def format(self, record):
-        record.exc_text = ""  # ensure formatException is called
-        return super(NoExceptionFormatter, self).format(record)
-
-    def formatException(self, record):
-        return ""
+        return result.replace("\n", "\r")
 
 
 class AppNameFilter(logging.Filter):
