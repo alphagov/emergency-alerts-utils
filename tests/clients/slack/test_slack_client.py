347f6bd2f5e75317a5d623c40fffbc718c8f8f46
Adam Mitchell <adam.mitchell@digital.cabinet-office.gov.uk>
2024-09-27 23:02:30 +0100

    Fix test reference

--- tests/clients/slack/test_slack_client.py
+++ tests/clients/slack/test_slack_client.py
@@ -13,18 +13,23 @@ def zendesk_client(app):
 
 
 def test_slack_client_send_message_to_slack(slack_client, app, mocker, rmock):
+    webhook_url = "fake-url"
+    subject = "Something has gone wrong"
+    message_type = "info"
+    markdown_sections = ["Description of what has gone wrong"]
+
     rmock.request(
         "POST",
-        "fake-url",
+        webhook_url,
         status_code=200,
         json={
             "attachments": [
                 {
                     "color": "#ff0000",
                     "blocks": [
-                        {"type": "header", "text": {"type": "plain_text", "text": "Something has gone wrong"}},
+                        {"type": "header", "text": {"type": "plain_text", "text": subject}},
                         {"type": "divider"},
-                        {"type": "section", "text": {"type": "mrkdwn", "text": "Description of what has gone wrong"}},
+                        {"type": "section", "text": {"type": "mrkdwn", "text": markdown_sections[0]}},
                     ],
                 }
             ]
@@ -33,21 +38,30 @@ def test_slack_client_send_message_to_slack(slack_client, app, mocker, rmock):
 
     mock_logger = mocker.patch.object(app.logger, "info")
 
-    message = SlackMessage()
+    message = SlackMessage(
+        webhook_url=webhook_url, subject=subject, message_type=message_type, markdown_sections=markdown_sections
+    )
     slack_client.send_message_to_slack(message)
 
     assert rmock.last_request.json() == message.request_data
     mock_logger.assert_called_once_with("Slack message sent successfully.")
 
 
-def test_zendesk_client_send_ticket_to_zendesk_error(zendesk_client, app, mocker, rmock):
+def test_slack_client_send_message_to_slack_error(slack_client, app, mocker, rmock):
+    webhook_url = "fake-url"
+    subject = "Something has gone wrong"
+    message_type = "info"
+    markdown_sections = ["Description of what has gone wrong"]
+
     rmock.request("POST", "fake-url", status_code=401, json={"foo": "bar"})
 
     mock_logger = mocker.patch.object(app.logger, "error")
 
-    ticket = SlackMessage({"something": "anything"})
+    message = SlackMessage(
+        webhook_url=webhook_url, subject=subject, message_type=message_type, markdown_sections=markdown_sections
+    )
 
     with pytest.raises(SlackError):
-        zendesk_client.send_ticket_to_zendesk(ticket)
+        slack_client.send_message_to_slack(message)
 
     mock_logger.assert_called_with("Slack message creation request failed with 401")
