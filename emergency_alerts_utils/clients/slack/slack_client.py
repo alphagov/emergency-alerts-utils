7c92d23cee492a7bb0a60bcbf7c3c6e64d8c73d9
Adam Mitchell <adam.mitchell@digital.cabinet-office.gov.uk>
2024-09-30 15:12:18 +0100

    Tweaks

--- emergency_alerts_utils/clients/slack/slack_client.py
+++ emergency_alerts_utils/clients/slack/slack_client.py
@@ -13,29 +13,30 @@ class SlackClient:
         self.webhook_url = None
 
     def send_message_to_slack(self, message):
+        self.message = message.request_data
         headers = {"Accept": "application/json"}
-        response = requests.post(message.webhook_url, json=message.request_data, headers=headers)
+        response = requests.post(message.webhook_url, json=self.message, headers=headers)
 
         if response.status_code != 200:
             current_app.logger.error(f"Slack message creation request failed with {response.status_code}")
             raise SlackError(response)
 
-        current_app.logger.info("Slack message sent successfully.")
+        current_app.logger.info("Slack message sent to the provided webhook URL.")
 
 
 class SlackMessage:
 
     message_status_colours = {"success": "#02b101", "error": "#e82b2a", "info": "#f5ca00", "general": "#68737d"}
 
-    @property
-    def message_colour(self):
-        return self.message_status_colours[self.message_type]
-
     def __init__(self, webhook_url, subject, message_type, markdown_sections):
         self.webhook_url = webhook_url
         self.subject = subject
         self.message_type = message_type
         self.markdown_sections = markdown_sections
+
+    @property
+    def message_colour(self):
+        return self.message_status_colours[self.message_type]
 
     @property
     def request_data(self):
