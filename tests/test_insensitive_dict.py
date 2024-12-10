8c8995d441cb75b32d38221f1e06b0b80098a111
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2024-12-10 11:46:27 +0000

    remove billing rates and recipients

--- tests/test_insensitive_dict.py
+++ tests/test_insensitive_dict.py
@@ -2,8 +2,7 @@ from functools import partial
 
 import pytest
 
-from emergency_alerts_utils.insensitive_dict import InsensitiveDict
-from emergency_alerts_utils.recipients import Cell, Row
+from emergency_alerts_utils.insensitive_dict import Cell, InsensitiveDict, Row
 
 
 def test_columns_as_dict_with_keys():
