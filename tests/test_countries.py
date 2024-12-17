215247b3edbb96a2c5dd18423d5c3cb96052dfde
Jonathan Owens <115698235+jonathan-owens-gds@users.noreply.github.com>
2024-12-17 09:08:26 +0000

    EAS-2502 Remove unused code (#90)
    * remove unused template code
    
    * remove billing rates and recipients
    
    * remove s3 module
    
    * remove markdown module
    
    * remove pdf and letter_timings modules
    
    * remove postal_address module
    
    * remove non-uk country data
    
    * remove redis and statsd clients
    
    * remove unused templates and statsd code
    
    * remove unused packages
    
    * change env var name
    
    * remove unused env var
    
    * fix tests
    
    * remove commented-out packages
    
    * remove unused properties
    
    * remove commented-out code
    
    * remove references to postage
    
    * remove commented-out code

--- tests/test_countries.py
+++ tests/test_countries.py
@@ -6,15 +6,12 @@ from emergency_alerts_utils.countries import (
     CountryNotFoundError,
 )
 from emergency_alerts_utils.countries.data import (
-    _EUROPEAN_ISLANDS_LIST,
     _UK_ISLANDS_LIST,
     ADDITIONAL_SYNONYMS,
     COUNTRIES_AND_TERRITORIES,
-    ROYAL_MAIL_EUROPEAN,
     UK,
     UK_ISLANDS,
     WELSH_NAMES,
-    Postage,
 )
 
 from .country_synonyms import ALL as ALL_SYNONYMS
@@ -33,24 +30,14 @@ def test_constants():
         ("Jethou", UK),
         ("Sark", UK),
     ]
-    assert Postage.EUROPE == "europe"
-    assert Postage.REST_OF_WORLD == "rest-of-world"
-    assert Postage.UK == "united-kingdom"
 
 
 @pytest.mark.parametrize("synonym, canonical", ADDITIONAL_SYNONYMS)
 def test_hand_crafted_synonyms_map_to_canonical_countries(synonym, canonical):
-    exceptions_to_canonical_countries = [
-        "Easter Island",
-        "South Georgia and the South Sandwich Islands",
-    ]
-
     synonyms = dict(COUNTRIES_AND_TERRITORIES).keys()
     canonical_names = list(dict(COUNTRIES_AND_TERRITORIES).values())
 
-    assert canonical in (
-        canonical_names + _EUROPEAN_ISLANDS_LIST + _UK_ISLANDS_LIST + exceptions_to_canonical_countries
-    )
+    assert canonical in (canonical_names + _UK_ISLANDS_LIST)
 
     assert synonym not in {CountryMapping.make_key(synonym_) for synonym_ in synonyms}
     assert Country(synonym).canonical_name == canonical
@@ -68,23 +55,14 @@ def test_all_synonyms():
 
 
 def test_crowdsourced_test_data():
-    for search, expected_country, expected_postage in CROWDSOURCED_MISTAKES:
-        if expected_country or expected_postage:
+    for search, expected_country in CROWDSOURCED_MISTAKES:
+        if expected_country:
             assert Country(search).canonical_name == expected_country
-            assert Country(search).postage_zone == expected_postage
 
 
 @pytest.mark.parametrize(
     "search, expected",
     (
-        ("u.s.a", "United States"),
-        ("america", "United States"),
-        ("United States America", "United States"),
-        ("ROI", "Ireland"),
-        ("Irish Republic", "Ireland"),
-        ("Rep of Ireland", "Ireland"),
-        ("RepOfIreland", "Ireland"),
-        ("deutschland", "Germany"),
         ("UK", "United Kingdom"),
         ("England", "United Kingdom"),
         ("Northern Ireland", "United Kingdom"),
@@ -95,32 +73,8 @@ def test_crowdsourced_test_data():
         ("NIR", "United Kingdom"),
         ("SCT", "United Kingdom"),
         ("WLS", "United Kingdom"),
-        ("gambia", "The Gambia"),
         ("Jersey", "United Kingdom"),
         ("Guernsey", "United Kingdom"),
-        ("Lubnān", "Lebanon"),
-        ("Lubnan", "Lebanon"),
-        ("ESPAÑA", "Spain"),
-        ("ESPANA", "Spain"),
-        ("the democratic people's republic of korea", "North Korea"),
-        ("the democratic peoples republic of korea", "North Korea"),
-        ("ALAND", "Åland Islands"),
-        ("Sao Tome + Principe", "Sao Tome and Principe"),
-        ("Sao Tome & Principe", "Sao Tome and Principe"),
-        ("Antigua, and Barbuda", "Antigua and Barbuda"),
-        ("Azores", "Azores"),
-        ("Autonomous Region of the Azores", "Azores"),
-        ("Canary Islands", "Canary Islands"),
-        ("Islas Canarias", "Canary Islands"),
-        ("Canaries", "Canary Islands"),
-        ("Madeira", "Madeira"),
-        ("Autonomous Region of Madeira", "Madeira"),
-        ("Região Autónoma da Madeira", "Madeira"),
-        ("Balearic Islands", "Balearic Islands"),
-        ("Islas Baleares", "Balearic Islands"),
-        ("Illes Balears", "Balearic Islands"),
-        ("Corsica", "Corsica"),
-        ("Corse", "Corsica"),
     ),
 )
 def test_hand_crafted_synonyms(search, expected):
@@ -147,25 +101,3 @@ def test_non_existant_countries(search, expected_error_message):
         Country(search)
     assert str(error.value) == repr(expected_error_message)
     assert isinstance(error.value, CountryNotFoundError)
-
-
-@pytest.mark.parametrize(
-    "search, expected",
-    (
-        ("u.s.a", "rest-of-world"),
-        ("Rep of Ireland", "europe"),
-        ("deutschland", "europe"),
-        ("UK", "united-kingdom"),
-        ("Jersey", "united-kingdom"),
-        ("Guernsey", "united-kingdom"),
-        ("isle-of-man", "united-kingdom"),
-        ("ESPAÑA", "europe"),
-    ),
-)
-def test_get_postage(search, expected):
-    assert Country(search).postage_zone == expected
-
-
-def test_euro_postage_zone():
-    for search in ROYAL_MAIL_EUROPEAN:
-        assert Country(search).postage_zone == Postage.EUROPE
