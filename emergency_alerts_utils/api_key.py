1c64915920982f28a52a37229a2dc91ca2ff3b90
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-02-24 15:48:43 +0000

    Add constants for admin approvals

---
+++ emergency_alerts_utils/api_key.py
@@ -0,0 +1,15 @@
+KEY_TYPE_NORMAL = "normal"
+KEY_TYPE_TEAM = "team"
+KEY_TYPE_TEST = "test"
+
+KEY_TYPE_DESCRIPTIONS = {
+    KEY_TYPE_NORMAL: "Live – sends to anyone",
+    KEY_TYPE_TEAM: "Team and guest list – limits who you can send to",
+    KEY_TYPE_TEST: "Test – pretends to send messages",
+}
+
+KEY_TYPES = [
+    KEY_TYPE_NORMAL,
+    KEY_TYPE_TEAM,
+    KEY_TYPE_TEST,
+]
