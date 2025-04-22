219c1610462049c503a137453e08b3d86c0684c2
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-04-22 12:34:25 +0100

    Transplant XML utility functions from CBC proxy

---
+++ emergency_alerts_utils/xml/cap.py
@@ -0,0 +1,113 @@
+# We use lxml in place of native xml to support consistent encoding/decoding,
+# handling of namespaces and for securely parsing untrusted XML
+from lxml import etree as ET
+
+from emergency_alerts_utils.xml.common import (
+    GOVERNMENT_CHANNEL,
+    OPERATOR_CHANNEL,
+    SENDER,
+    SEVERE_CHANNEL,
+    xml_subelement,
+)
+
+
+def generate_cap_link_test(
+    identifier,
+    sent,
+):
+    alert = ET.Element(
+        "alert",
+        attrib={
+            "xmlns": "urn:oasis:names:tc:emergency:cap:1.2",
+        },
+    )
+
+    xml_subelement(alert, "identifier", text=identifier)
+    xml_subelement(alert, "sender", text=SENDER)
+    xml_subelement(alert, "sent", text=sent.astimezone().isoformat())
+    xml_subelement(alert, "status", text="Test")
+    xml_subelement(alert, "msgType", text="Alert")
+    xml_subelement(alert, "scope", text="Public")
+
+    return alert
+
+
+def generate_cap_alert(
+    identifier,
+    headline,
+    description,
+    areas,
+    sent,
+    expires,
+    language,
+    channel,
+):
+    alert = ET.Element(
+        "alert",
+        attrib={
+            "xmlns": "urn:oasis:names:tc:emergency:cap:1.2",
+        },
+    )
+
+    xml_subelement(alert, "identifier", text=identifier)
+    xml_subelement(alert, "sender", text=SENDER)
+    xml_subelement(alert, "sent", text=sent)
+    xml_subelement(alert, "status", text="Actual")
+    xml_subelement(alert, "msgType", text="Alert")
+    xml_subelement(alert, "scope", text="Public")
+
+    info = xml_subelement(alert, "info")
+
+    xml_subelement(info, "language", text=language)
+    xml_subelement(info, "category", text="Safety")
+    # List of potential channel category values is defined in
+    # https://docs.google.com/spreadsheets/d/1NzaIa3BQoZzYVFXkhS2lSyGXLMRlI0Dg/edit#gid=316184578 for details
+    cap_event = "RMT"
+    if channel == OPERATOR_CHANNEL:
+        cap_event = "OPR"
+    if channel == SEVERE_CHANNEL:
+        cap_event = "Alert"
+    elif channel == GOVERNMENT_CHANNEL:
+        cap_event = "EAN"
+    xml_subelement(info, "event", text=cap_event)
+    xml_subelement(info, "urgency", text="Expected")
+    xml_subelement(info, "severity", text="Severe")
+    xml_subelement(info, "certainty", text="Likely")
+    xml_subelement(info, "expires", text=expires)
+    xml_subelement(info, "senderName", text="GOV.UK Emergency Alerts")
+    xml_subelement(info, "headline", text=headline)
+    xml_subelement(info, "description", text=description)
+
+    for i, a in enumerate(areas):
+        area = xml_subelement(info, "area")
+
+        xml_subelement(area, "areaDesc", text="area-{}".format(i + 1))
+        xml_subelement(
+            area,
+            "polygon",
+            text=" ".join(["{},{}".format(pair[0], pair[1]) for pair in a["polygon"]]),
+        )
+
+    return alert
+
+
+def generate_cap_cancel_message(identifier, sent, references):
+    alert = ET.Element(
+        "alert",
+        attrib={
+            "xmlns": "urn:oasis:names:tc:emergency:cap:1.2",
+        },
+    )
+
+    references_list = [f"{SENDER},{ref['message_id']},{ref['sent']}" for ref in references]
+    references_string = " ".join(references_list)
+
+    xml_subelement(alert, "identifier", text=identifier)
+    xml_subelement(alert, "sender", text=SENDER)
+    xml_subelement(alert, "sent", text=sent)
+    xml_subelement(alert, "status", text="Actual")
+    xml_subelement(alert, "msgType", text="Cancel")
+    xml_subelement(alert, "scope", text="Public")
+    xml_subelement(alert, "references", text=references_string)
+
+    return alert
---
+++ emergency_alerts_utils/xml/common.py
@@ -0,0 +1,88 @@
+# Shared utilties and constants between CAP and IBAG broadcast formats
+
+from lxml import etree as ET
+from signxml import XMLSigner
+
+ALERT_MESSAGE_TYPE = "Alert"
+UPDATE_MESSAGE_TYPE = "Update"
+CANCEL_MESSAGE_TYPE = "Cancel"
+TEST_MESSAGE_TYPE = "Test"
+
+IBAG_MESSAGE_FORMAT = "IBAG"
+CAP_MESSAGE_FORMAT = "CAP"
+
+TEST_CHANNEL = "test"
+OPERATOR_CHANNEL = "operator"
+SEVERE_CHANNEL = "severe"
+GOVERNMENT_CHANNEL = "government"
+# Channels we allow the user to request the broadcast is sent on. We will then transform
+# these into the XML needed to get sent on the 4380 RMT test channel, the 4382 operator
+# channel the 4378 severe channel or the 4370 government channel.
+#
+# This won't allow the choice of any of the test, operator, severe or government channels
+# (there are multiple) but instead to go out on one of the four channels we've configured.
+ALLOWED_CHANNELS = [TEST_CHANNEL, OPERATOR_CHANNEL, SEVERE_CHANNEL, GOVERNMENT_CHANNEL]
+
+SENDER = "broadcasts@notifications.service.gov.uk"
+
+
+def digitally_sign(xml, key, cert):
+    """
+    Given an xml etree, envelopes a Signature block to the root element, that consists of a digital signature based on
+    https://www.ietf.org/rfc/rfc4051.txt. Canonicalizes the element using xml-c14n11, and then signs that, then returns
+    the xml etree with that signature in place.
+    """
+    signed_root = XMLSigner(
+        signature_algorithm="rsa-sha256",
+        digest_algorithm="sha256",
+        c14n_algorithm="http://www.w3.org/2006/12/xml-c14n11",
+    ).sign(xml, key=key, cert=cert)
+
+    return signed_root
+
+
+def xml_subelement(elem, name, attrib=None, text=None):
+    if attrib is None:
+        attrib = {}
+
+    sub = ET.SubElement(elem, name, attrib=attrib)
+
+    if text is not None:
+        sub.text = text
+
+    return sub
+
+
+def convert_etree_to_string(xml):
+    """
+    Currently doesn't canonicalise it, as we believe this may be causing issues with line breaks in the description
+    that users see.
+    """
+    return ET.tostring(xml, encoding="unicode")
+
+
+def validate_message_format(message_format):
+    """validates the message format to be sent to the CBC"""
+
+    return {
+        "ibag": IBAG_MESSAGE_FORMAT,
+        "cap": CAP_MESSAGE_FORMAT,
+    }[message_format.lower()]
+
+
+def validate_message_type(message_type):
+    """validate_message_type returns a KeyError if the alert type is not recognised"""
+
+    return {
+        "alert": ALERT_MESSAGE_TYPE,
+        "update": UPDATE_MESSAGE_TYPE,
+        "cancel": CANCEL_MESSAGE_TYPE,
+        "test": TEST_MESSAGE_TYPE,
+    }[message_type.lower()]
+
+
+def validate_channel(channel):
+    """validate_channel asserts if the channel is not recognised"""
+    if channel not in ALLOWED_CHANNELS:
+        raise AssertionError(f"'{channel}' is not an allowed channel. Must be in {ALLOWED_CHANNELS}")
+    return channel
